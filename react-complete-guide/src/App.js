0ddaa90c240aa5deb89b8d3032bc189a692f993c
cristhian Forero <cristhianforerod@gmail.com>
2020-06-14 16:53:58 -0500

    Add new topic: List manipulation

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -40,6 +40,12 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  deletePersonHandler = (index) => {
+    const persons = this.state.persons;
+    persons.splice(index, 1);
+    this.setState({ persons });
+  };
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -54,16 +60,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Cristhian!')}
-            changed={this.nameChangeHandler.bind(this)}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          {this.state.persons.map((person, index) => {
+            return <Person name={person.name} age={person.age} key={index} click={() => this.deletePersonHandler(index)} />;
+          })}
         </div>
       );
     }
