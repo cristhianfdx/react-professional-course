eaebee09a90612394d8f7c4cd8d515f6725141a8
cristhian Forero <cristhianforerod@gmail.com>
2020-06-14 16:17:51 -0500

    Add new topics: Conditional JSX content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
         { name: 'Cristhian', age: 31 },
         { name: 'Stephanie', age: 26 },
       ],
+      showPersons: false,
     };
   }
 
@@ -34,31 +35,40 @@ class App extends Component {
     });
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
     };
 
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
-        <button style={style} onClick={this.switchNameHandler.bind(this, 'Lucy')}>
+        <button style={style} onClick={this.togglePersonsHandler}>
           Switch Name
         </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Cristhian!')}
-          changed={this.nameChangeHandler.bind(this)}
-        >
-          My Hobbies:{' '}
-        </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        {this.state.showPersons === true ? (
+          <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Cristhian!')}
+              changed={this.nameChangeHandler.bind(this)}
+            >
+              My Hobbies: Racing
+            </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          </div>
+        ) : null}
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -7,7 +7,7 @@ const person = (props) => {
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
-      <p>{props.children}</p>
+      {props.children ? <p>{props.children}</p> : null}
       <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
