5cc0b611d7a4ab9628b7501927706813baf3f36d
cristhian Forero <cristhianforerod@gmail.com>
2020-06-14 16:24:15 -0500

    Add new topics: Dynamic Javascript content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
-  }
+  };
 
   render() {
     const style = {
@@ -49,13 +49,33 @@ class App extends Component {
       cursor: 'pointer',
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Cristhian!')}
+            changed={this.nameChangeHandler.bind(this)}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <button style={style} onClick={this.togglePersonsHandler}>
-          Switch Name
+          Toggle Persons
         </button>
-        {this.state.showPersons === true ? (
+        {persons}
+        {/* {this.state.showPersons === true ? (
           <div>
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
             <Person
@@ -68,7 +88,7 @@ class App extends Component {
             </Person>
             <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
           </div>
-        ) : null}
+        ) : null} */}
       </div>
     );
   }
